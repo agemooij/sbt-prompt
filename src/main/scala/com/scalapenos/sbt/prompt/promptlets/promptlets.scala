d44d101f9e4f21990802ecad83a5b1c3b03875a5
Age Mooij <age.mooij@gmail.com>
2014-11-09 22:03:05 +0100

    Simplified definitions of the standard promptlets a bit based on the example code written for the README

--- src/main/scala/com/scalapenos/sbt/prompt/promptlets/promptlets.scala
+++ src/main/scala/com/scalapenos/sbt/prompt/promptlets/promptlets.scala
@@ -13,16 +13,16 @@ trait BasicPromptlets extends Styles {
   def text(content: String, style: Style): Promptlet = text(_ ⇒ content, style)
   def text(content: State ⇒ String, style: Style): Promptlet = Promptlet(state ⇒ StyledText(content(state), style))
 
-  def currentProject(style: Style = NoStyle): Promptlet = Promptlet(state ⇒ {
+  def currentProject(style: Style = NoStyle): Promptlet = text(state ⇒ {
     val extracted = Project.extract(state)
     val project = extracted.currentRef.project
     val root = extracted.rootProject(extracted.currentRef.build)
 
-    StyledText(if (project == root) project else s"${root}/${project}", style)
-  })
+    if (project == root) project else s"${root}/${project}"
+  }, style)
 
-  def userName(style: Style = NoStyle): Promptlet = Promptlet(state ⇒ StyledText(cachedUserName, style))
-  def hostName(style: Style = NoStyle): Promptlet = Promptlet(state ⇒ StyledText(cachedHostName, style))
+  def userName(style: Style = NoStyle): Promptlet = text(_ ⇒ cachedUserName, style)
+  def hostName(style: Style = NoStyle): Promptlet = text(_ ⇒ cachedHostName, style)
 
   private lazy val cachedUserName = sys.props("user.name")
   private lazy val cachedHostName = java.net.InetAddress.getLocalHost().getHostName().stripSuffix(".local")
